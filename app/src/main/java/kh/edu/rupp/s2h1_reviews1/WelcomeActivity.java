b7b81f74d25545ac0d621e31dcb01af8cc635dc0
Tola Dev <lonwtola@gmail.com>
2026-02-07 16:18:04 +0700

    feat: Add welcome screen
    Adds a new welcome screen as the initial launcher activity.
    
    This includes:
    - Creating the `WelcomeActivity` and its corresponding layout file.
    - Adding a welcome icon, title, description text, and a start button to the layout.
    - Defining new string resources for the welcome screen content.
    - Updating the `AndroidManifest.xml` to set `WelcomeActivity` as the launcher activity.
    - Modifying the app theme to use `NoActionBar` for a full-screen experience.

---
+++ app/src/main/java/kh/edu/rupp/s2h1_reviews1/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package kh.edu.rupp.s2h1_reviews1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
